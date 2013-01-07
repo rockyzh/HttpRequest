8c855564bdc8410ace86c2ff77a7ff2e1e625b74
Brian <bperin42@gmail.com>
2013-01-07 02:06:41 -0800

    Made plugin runnable on new thread
    Changed plugin stucture to not block the WebCore thread

--- com/phonegap/plugins/http/HttpRequestPlugin.java
+++ com/phonegap/plugins/http/HttpRequestPlugin.java
@@ -22,77 +22,90 @@ public class HttpRequestPlugin extends CordovaPlugin {
 	private HttpRequest request;
 
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+	public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) {
 
-		try {
+		if (action.equals("execute")) {
 
-			String url = args.getString(0);
-			String method = args.getString(1);
+			cordova.getThreadPool().execute(new Runnable() {
 
-			JSONObject params = args.getJSONObject(2);
-			JSONObject options = args.getJSONObject(3);
+				public void run() {
+					try {
 
-			if (action.equals("execute")) {
+						String url = args.getString(0);
+						String method = args.getString(1);
 
-				if (method.equals("post")) {
-					this.setPostUrl(url);
+						JSONObject params = args.getJSONObject(2);
+						JSONObject options = args.getJSONObject(3);
+
+						if (method.equals("post")) {
+							setPostUrl(url);
+						}
+						else if (method.equals("get")) {
+							setGetUrl(url, params);
+						}
+
+						if (options.getBoolean("acceptGzip") == true) {
+							acceptGzip();
+						}
+						if (params.length() > 0 && method.equals("post")) {
+							setPostParams(params);
+						}
+						if (options.getBoolean("trustAll") == true) {
+							acceptAll();
+						}
+
+						int code = getCode();
+						String body = getBody();
+						String msg = getMessage();
+						disconnect();
+
+						JSONObject response = new JSONObject();
+						
+						response.put("code", code);
+						response.put("body", body);
+						response.put("message", msg);
+						
+
+//						Log.d(TAG, "Response from " + url + " " + code);
+//						Log.d(TAG, "Response body " + url + " " + body);
+
+						if (code == 200) {
+							callbackContext.success(response);
+							return;
+						}
+						else {
+							callbackContext.error(response);
+							return;
+						}
+
+					}
+					catch (JSONException ex) {
+						Log.e(TAG, ex.getMessage());
+					}
+					catch (HttpRequestException ex) {
+						Log.e(TAG, ex.getMessage());
+					}
+					catch (Exception ex) {
+						Log.e(TAG, ex.getMessage());
+					}
+
 				}
-				else if (method.equals("get")) {
-					this.setGetUrl(url, params);
-				}
-
-				if (options.getBoolean("acceptGzip") == true) {
-					this.acceptGzip();
-				}
-				if (params.length() > 0 && method.equals("post")) {
-					this.setPostParams(params);
-				}
-				if (options.getBoolean("trustAll") == true) {
-					this.acceptAll();
-				}
-
-				int code = this.getCode();
-				String body = this.getBody();
-				String msg = this.getMessage();
-
-				JSONObject response = new JSONObject();
-				response.put("code", code);
-				response.put("body", body);
-				response.put("message", msg);
-
-				Log.d(TAG, "Response from " + url + " " + code);
-
-				if (code == 200) {
-					callbackContext.success(response);
-					return true;
-				}
-				else {
-					callbackContext.error(response);
-					return false;
-				}
-
-			}
-
+			});
+			return true;
 		}
-		catch (JSONException ex) {
-			Log.e(TAG, ex.getMessage());
-		}
-		catch (HttpRequestException ex) {
-			Log.e(TAG, ex.getMessage());
-		}
-		catch (Exception ex) {
-			Log.e(TAG, ex.getMessage());
-		}
-
 		return false;
+
 	}
 
-	private void setPostUrl(String url) throws HttpRequestException {
+	public void disconnect(){
+		request.disconnect();
+	}
+	public void setPostUrl(String url) throws HttpRequestException {
 		request = HttpRequest.post(url);
 		return;
 	}
 
-	private void setGetUrl(String url, JSONObject params) throws HttpRequestException, JSONException {
+	public void setGetUrl(String url, JSONObject params) throws HttpRequestException, JSONException {
 
 		Map<String, String> inputParams = new HashMap<String, String>();
 
@@ -112,11 +125,11 @@ public class HttpRequestPlugin extends CordovaPlugin {
 			}
 		}
 
-		request = HttpRequest.get(url,inputParams,true);
+		request = HttpRequest.get(url, inputParams, true);
 		return;
 	}
 
-	private void setPostParams(JSONObject params) throws JSONException, HttpRequestException {
+	public void setPostParams(JSONObject params) throws JSONException, HttpRequestException {
 
 		Map<String, String> inputParams = new HashMap<String, String>();
 
@@ -136,7 +149,7 @@ public class HttpRequestPlugin extends CordovaPlugin {
 		return;
 	}
 
-	private void acceptAll() throws HttpRequestException {
+	public void acceptAll() throws HttpRequestException {
 		// Accept all certificates
 		request.trustAllCerts();
 		// Accept all hostnames
@@ -144,21 +157,21 @@ public class HttpRequestPlugin extends CordovaPlugin {
 		return;
 	}
 
-	private void acceptGzip() {
+	public void acceptGzip() {
 		// Tell server to gzip response and automatically uncompress
 		request.acceptGzipEncoding().uncompress(true);
 		return;
 	}
 
-	private int getCode() throws HttpRequestException {
+	public int getCode() throws HttpRequestException {
 		return request.code();
 	}
 
-	private String getMessage() throws HttpRequestException {
+	public String getMessage() throws HttpRequestException {
 		return request.message();
 	}
 
-	private String getBody() throws HttpRequestException {
+	public String getBody() throws HttpRequestException {
 		return request.body();
 	}
 
